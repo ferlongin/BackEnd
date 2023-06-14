abbf04dba32e0a57e9fb6e20ce22f6274a59cc7a
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-06-14 18:00:10 -0300

    genSalt bycript

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const express = require('express');
-const {dbConnection} = require('./src/db/config');
-
+const { dbConnection } = require('./src/db/config');
+const bcrypt = require('bcrypt'); // Importar bcrypt
 
 const app = express();
 dbConnection();
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,8 +15,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/usuarios", require("./src/routes/usuarios.routes"));
 
-app.listen(process.env.PORT, () =>{
-    console.log('App listening on PORT: '+process.env.PORT);
-})
+app.listen(process.env.PORT, () => {
+  console.log('App listening on PORT: ' + process.env.PORT);
+  
+  // Generar el salt y mostrarlo por consola
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Salt:', salt);
+  });
+});
 
 module.exports = app;
