946f3ae83e022cf6df3c5cfe6184b81e88395ecf
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-06-24 20:44:48 -0300

    install cors

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const cors = require('cors');
+
 const express = require('express');
 const { dbConnection } = require('./src/db/config');
 const bcrypt = require('bcrypt'); // Importar bcrypt
@@ -8,6 +10,7 @@ dbConnection();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.status(200).send({ message: "You are connected to the project" });
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,6 +2,8 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 const usuariosController = require("../controllers/usuarios.controller");
 const checkFields = require("../middlewares/validateFields");
+const cors = require('cors');
+
 
 const router = Router();
 
@@ -23,6 +25,7 @@ router.post(
 
 router.post(
   "/login",
+  cors(),
   [
     check("email").not().isEmpty(),
     check("password").not().isEmpty(),
